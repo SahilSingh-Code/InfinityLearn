90d0af3d0729c50b42e5c630aa083c660dbf47c3
SahilSingh <108088126+SahilSingh-Code@users.noreply.github.com>
2026-04-26 16:08:20 -0700

    IFLEARN-4 Logging (#15)

---
+++ Core/Logging.h
@@ -0,0 +1,55 @@
+//-----------------------------------------------------------------------------
+// Logging.h
+//
+// This is InfinityLearn's logging system. It provides a simple interface for
+// logging messages to the console and to files.
+//
+// Which level of messages are printed depends on the build type, which can be
+// set at compile time by using the flag -DCMAKE_BUILD_TYPE=<BuildType>
+//
+// Here are the default log levels for each build type:
+// - DEBUG: DEBUG, INFO, WARNING, ERROR
+// - DEVELOP: INFO, WARNING, ERROR
+// - RELEASE: INFO, WARNING, ERROR
+// - PERFORMANCE: PERFORMANCE, WARNING, ERROR
+//-----------------------------------------------------------------------------
+
+#pragma once
+
+// Default the active log level to INFO if it is not defined
+#ifndef IFLEARN_ACTIVE_LOG_LEVEL
+#define IFLEARN_ACTIVE_LOG_LEVEL INFO
+#endif
+
+// Includes
+#include <string>  //  For std::string
+
+
+namespace InfinityLearn
+{
+
+// The log level enumeration defining the log severity
+enum LogLevel
+{
+    PERFORMANCE,
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR
+};
+
+/// <summary>
+/// Logs a message with the specified logging level.
+/// </summary>
+/// <param name="level">The severity level of the log message.</param>
+/// <param name="message">The message to log.</param>
+void Log(LogLevel level, const std::string& message);
+
+/// <summary>
+/// Writes a message to a specified file.
+/// </summary>
+/// <param name="message">The message to write to the file.</param>
+/// <param name="filename">The name of the file to write to.</param>
+void LogToFile(const std::string& message, const std::string& filename);
+
+}  // namespace InfinityLearn
